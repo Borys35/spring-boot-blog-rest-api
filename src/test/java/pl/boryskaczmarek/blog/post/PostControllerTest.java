77e5c4c71c4f00758ae7668d64bfade4798c51c2
Borys35 <boryskac10@gmail.com>
2024-10-30 12:42:35 +0100

    Create PostController test class

--- src/test/java/pl/boryskaczmarek/blog/post/PostControllerTest.java
+++ src/test/java/pl/boryskaczmarek/blog/post/PostControllerTest.java
@@ -1,10 +1,17 @@
 package pl.boryskaczmarek.blog.post;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,8 +25,27 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class PostControllerTest {
 
+    @LocalServerPort
+    int serverPort;
+
+    RestClient client;
+
     @Container
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
 
+    @BeforeEach
+    void setUp() {
+        client = RestClient.create("http://localhost:" + serverPort);
+    }
+
+    @Test
+    void shouldFindAllPosts() {
+        List<Post> posts = client.get()
+                .uri("/api/posts")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+
+        assertEquals(5, posts.size());
+    }
 }
